6613372b60b108010bb7d87ac3eae62ca302ff96
Debosmit Choudhury <debosmit.choudhury@enggproject.com>
2022-04-01 18:13:45 +0530

    Hero component added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Resume from "../assests/Debosmit_Resume.pdf";
+// import BGK from "../assests/liquid-cheese.svg";
+import IMG from "../assests/pic.jpeg";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div className="wrapper h-screen justify-around flex-col lg:flex-row flex p-10 relative drop-shadow-lg   ">
+      <div className="left flex flex-col justify-around lg:w-3/4 p-4">
+        <div className="name flex flex-col gap-4 z-10">
+          <div className="name flex flex-col">
+            <span className="text-3xl lg:text-5xl font-bold ">Hey! I am</span>
+            <span className="text-3xl lg:text-5xl font-bold">Debosmit Choudhury</span>
+          </div>
+          <span>
+            Pursuing BTech from Asansol Engineering College and also With an
+            urge of learning new technology and new web-based applications,
+            started working of self-made projects using a tech stack consisting
+            of ReactJS, MongoDB and NodeJS
+          </span>
+          <div className="contact m-4 text-2xl ">
+            <button className="border hover:border-red-300 rounded-xl hover:rounded-2xl px-3 py-1 bg-gradient-to-b from-sky-500  active:scale-95 to-indigo-600 text-white transition-all duration-200">
+              Hire me!
+            </button>
+          </div>
+        </div>
+        <div className="icons text-3xl lg:text-5xl flex p-6 gap-4 ">
+          <div className="github cursor-pointer hover:text-indigo-600  transition-all duration-200 ">
+            <FaGithub />
+          </div>
+          <div className="linkedin cursor-pointer hover:text-indigo-600 transition-all duration-200  ">
+            <FaLinkedin />
+          </div>
+          <div className="Twitter cursor-pointer hover:text-indigo-600 transition-all duration-200 ">
+            <FaTwitter />
+          </div>
+        </div>
+      </div>
+      <div className="right flex justify-center items-center object-contain relative transition-all duration-300">
+        <div className="projects border-2 border-white absolute right-16 text-xl font-bold p-4 rounded-2xl hidden lg:block lg:top-16 hover:border-indigo-600 drop-shadow-lg z-10 cursor-pointer active:scale-90 transition-all duration-300 backdrop-blur-xl  bg-white/30">
+          Check out Projects
+        </div>
+        <div className="projects border-2 border-white absolute left-16 text-xl font-bold p-4 rounded-2xl  bottom-16 hover:border-indigo-600 drop-shadow-lg z-10 cursor-pointer active:scale-90 transition-all duration-300 hidden lg:block backdrop-blur-xl  bg-white/30">
+          <a href={Resume} download>To Know More</a> 
+        </div>
+        <img
+          className="w-3/4 rounded-full drop-shadow-lg "
+          src={IMG}
+          alt="Debosmit"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrFormClose } from "react-icons/gr";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState("backdrop-blur-none");
+  const [getMenu, setGetMenu] = useState(false);
+
+  window.addEventListener("scroll", () =>
+    setNavbar(window.scrollY >= 128 ? "backdrop-blur-xl" : "backdrop-blur-none")
+  );
   return (
-    <div className="wrapper flex h-20 justify-between mb-3 top-0">
+    <div
+      className={`wrapper flex h-20 ${
+        getMenu ? "h-screen " : "h-20"
+      } justify-between mb-3 top-0 fixed z-20  w-full ${navbar} transition-all duration-300 `}
+    >
       <div className="left flex-1 items-center flex ">
-        <div className="name font-bold text-2xl cursor-pointer p-4">
+        <div className="name font-bold text-2xl cursor-pointer p-6">
           <h1>Debosmit</h1>
         </div>
       </div>
-      <div className="right  items-center flex justify-center font-medium transition-all duration-300 ">
-        <div className="menu hidden lg:block">
-          <ul className="list-none flex gap-6 cursor-pointer ">
-            <li className="transition-all duration-200 active:scale-95 group ">
+      <div className="right h-full lg:h-20 items-center flex justify-end  font-medium transition-all duration-300 ">
+        <div
+          className={`menu h-screen w-screen py-4 transition-all absolute ${
+            !getMenu ? "top-full" : "top-0"
+          } lg:static lg:h-auto lg:w-auto lg:text-lg z-10 lg:bg-transparent bg-black text-white lg:text-black  `}
+        >
+          <ul className="list-none flex gap-6 cursor-pointer h-auto lg:flex-row flex-col justify-center items-center overflow-hidden ">
+            <li
+              className="transition-all duration-200 active:scale-95 group "
+              onClick={() => setGetMenu(false)}
+            >
               Home
               <div class="h-0.5 w-0 group-hover:w-full bg-indigo-700 transition-all"></div>
             </li>
-            <li className="transition-all duration-200 active:scale-95 group">
+            <li
+              className="transition-all duration-200 active:scale-95 group"
+              onClick={() => setGetMenu(false)}
+            >
               Services
               <div class="h-0.5 w-0 group-hover:w-full bg-indigo-700 transition-all"></div>
             </li>
-            <li className="transition-all duration-200 active:scale-95 group">
+            <li
+              className="transition-all duration-200 active:scale-95 group"
+              onClick={() => setGetMenu(false)}
+            >
               Experiences
               <div class="h-0.5 w-0 group-hover:w-full bg-indigo-700 transition-all"></div>
             </li>
-            <li className="transition-all duration-200 active:scale-95 group">
+            <li
+              className="transition-all duration-200 active:scale-95 group"
+              onClick={() => setGetMenu(false)}
+            >
               Testimonials
               <div class="h-0.5 w-0 group-hover:w-full bg-indigo-700 transition-all"></div>
             </li>
+            <div className="contact p-8 hidden md:block ">
+              <button className="border hover:border-red-300 rounded-xl hover:rounded-2xl px-3 py-1 bg-gradient-to-b from-sky-500  active:scale-95 to-indigo-600 text-white transition-all duration-200 z-50">
+                Contact
+              </button>
+            </div>
           </ul>
         </div>
-        <div className="contact p-8   ">
-          <button className="border hover:border-red-300 rounded-xl hover:rounded-2xl px-3 py-1 bg-gradient-to-b from-sky-500  active:scale-95 to-indigo-600 text-white transition-all duration-200">
-            Contact
-          </button>
+        <div
+          className="toggle lg:hidden block px-8 text-2xl lg:text-black text-white transition-all duration-300"
+          onClick={() => setGetMenu(!getMenu)}
+        >
+          {getMenu ? <GiHamburgerMenu /> : <GrFormClose />}
         </div>
       </div>
     </div>
